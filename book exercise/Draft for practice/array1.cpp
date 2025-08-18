96507e310bdaa4851501e589f0be7befd0f3ee14
yoyorm <yoyormiau1011@gmail.com>
2025-08-18 20:00:58 +0800

    测试：熟悉github提交功能

--- book exercise/Draft for practice/array1.cpp
+++ book exercise/Draft for practice/array1.cpp
@@ -2,10 +2,11 @@
 int main()
 {
 	using namespace std;
+	cout << "please type the pocket number you want:";
 	int packet[3] = { 1,2,3 };
 	int cost[3] = {10,20,30};
 	int x = 0;
 	cin >> x;
-	cout << "the price is" << packet[x - 1] * cost[x - 1] << " yuan";
+	cout << "the price is " << packet[x - 1] * cost[x - 1] << " yuan";
 	return 0;
 }
