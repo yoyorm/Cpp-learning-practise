50edb726f488d1511bba9cc603d8ff62a6a46b45
yoyorm <yoyormiau1011@gmail.com>
2025-08-19 12:58:13 +0800

    更新：array1 输入字符串草稿

--- book exercise/Draft for practice/array1.cpp
+++ book exercise/Draft for practice/array1.cpp
@@ -13,24 +13,58 @@
 //}
 
 //数组中使用字符串
+//int main()
+//{
+//	using namespace std;
+//	const int size = 10;
+//	char name1[size];
+//	char name2[size]="yoyorm";
+//	cout << "my name is " << name2 << endl;
+//	cout << "What's your name?\n";
+//	cin >> name1;
+//	cout << "Well, " << name1 << ", your name has been stored.\n";
+//	cout << "your name has " << strlen(name1) << " letters.\n";
+//	cout << "your first letter is " << name1[0] << endl;
+//	cout << "your name is in an array that owns " << sizeof(name1) << " bytes.\n";
+//	name2[4] = '\0';
+//	cout << "my first 4 letters are " << name2;
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+//使用cin.getline/get()
+//cin读取到空白字符后结束
+//
+//int main()
+//{
+//	using namespace std;
+//	const int ArSize = 20;
+//	char name[ArSize];
+//	char dessert[ArSize];
+//	
+//	cout << "Enter your name:\n";
+//	cin.get(name,ArSize).get();
+//	cout << "Enter your favorite dessert:\n";
+//	cin.get(dessert,ArSize).get();
+//	cout << dessert << "for you, " << name;
+//
+//	return 0;
+//}
+
 int main()
 {
 	using namespace std;
-	const int size = 10;
-	char name1[size];
-	char name2[size]="yoyorm";
-	cout << "my name is " << name2 << endl;
-	cout << "What's your name?\n";
-	cin >> name1;
-	cout << "Well, " << name1 << ", your name has been stored.\n";
-	cout << "your name has " << strlen(name1) << " letters.\n";
-	cout << "your first letter is " << name1[0] << endl;
-	cout << "your name is in an array that owns " << sizeof(name1) << " bytes.\n";
-	name2[4] = '\0';
-	cout << "my first 4 letters are " << name2;
-
-
-
+	int year = 0;
+	char name[20];
+	cout << "type your year:\n";
+	cin >> year;
+	cout << "tpye your name\n";
+	cin.getline(name, 20);
+	cout << "your name is: " << name << "\nyour year is: " << year;
 
 	return 0;
 }
