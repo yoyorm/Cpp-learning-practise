7e3dba6f8b30c5e330fc38a1d6adfce378419bb9
yoyorm <yoyormiau1011@gmail.com>
2025-08-22 18:09:38 +0800

    更新：增加了第5章 循环 的部分练习

---
+++ book exercise/Cpt5 Circulation/1 3 sum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+//int main()
+//{
+//	int x, y;
+//	cout << "Enter the first num: ";
+//	cin >> x;
+//	cout << "Enter the second num: ";
+//	cin >> y;
+//	int result=0;
+//	for (int temp = x; temp <= y; temp++)
+//	{
+//		result += temp;
+//	}
+//	cout << "sum = " << result;
+//}
+
+//对用户的所有输入求和，遇到0结束
+//int main()
+//{
+//	long sum = 0;
+//	long x;
+//	cout << "Enter the num:"<<endl;
+//	cin>>x;
+//	while (x != 0)
+//	{
+//		sum += x;
+//		cout << "Sum = " << sum<<endl;
+//		cin >> x;
+//		
+//	}
+//	return 0;
+//
+//}
---
+++ book exercise/Cpt5 Circulation/4 investment.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//x,y初始100元
+//x 单利10% ；y复利5%,看看多少年后y超越x
+
+//int main()
+//{
+//	double rate1 = 0.1;
+//	double rate2 = 0.05 ;
+//	double x = 100, y = 100;
+//	double x_s = rate1 * x;
+//	int year = 0;
+//	while (y <= x)
+//	{
+//		x += x_s;
+//		y *= 1 + rate2;
+//		year++;
+//	}
+//	cout << "after "<<year<<" years"<<" y>x!\n";
+//	cout << "x = " << x << "\t" << "y = " << y;
+//	return 0;
+//}
