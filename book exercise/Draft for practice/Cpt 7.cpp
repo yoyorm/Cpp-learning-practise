4ee65973ca0d46e9b389b8fd3187fe7fbbb3994f
yoyorm <yoyormiau1011@gmail.com>
2025-08-31 11:10:59 +0800

    更新：增加了第 7 章 递归的草稿

--- book exercise/Draft for practice/Cpt 7.cpp
+++ book exercise/Draft for practice/Cpt 7.cpp
@@ -33,34 +33,106 @@ using namespace std;
 
 //返回c风格字符串的函数
 //将问题转化为返回char类型的指针（记得末尾加上\0）
-char* buildstr(char ch, int times);
+//char* buildstr(char ch, int times);
+//int main()
+//{
+//	char ch;
+//	cout << "Enter a char: ";
+//	cin >> ch;
+//	int times;
+//	cout << "Enter times: ";
+//	cin >> times;
+//	char* ps = buildstr(ch, times);
+//	cout << endl << ps;
+//	delete[] ps;
+//	ps = buildstr('+', times/2);
+//	cout << ps << "-DONE-" << ps;
+//	delete[] ps;
+//	return 0;
+//}
+//
+//char* buildstr(char ch,int times)
+//{
+//	char* temp = new char[times + 1];
+//	//为指针开辟内存，方便后续指针传递返回出去
+//	int i = 0;
+//	for (i; i < times; i++)
+//	{
+//		temp[i] = ch;
+//	}
+//	temp[times] = '\0';
+//	cout << endl;
+//	return(temp);
+//}
+
+
+
+
+//倒计时
+//void countdown(int n);
+//int main()
+//{
+//	countdown(4);
+//	return 0;
+//}
+//void countdown(int n)
+//{
+//	cout << "count down...." << n << endl;
+//	if (n > 0)
+//	{
+//		n--;
+//		countdown(n);
+//		cout << n << endl;
+//	}
+//}
+
+
+
+
+//输出图像：如用递归
+//|                                                               |
+//|                               |                               |
+//|               |               |               |               |
+//|       |       |       |       |       |       |       |       |
+//|   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+//| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+void subdivide(char ch[], int first, int end, int times);
+const int len = 66;
+const int times = 6;
 int main()
 {
-	char ch;
-	cout << "Enter a char: ";
-	cin >> ch;
-	int times;
-	cout << "Enter times: ";
-	cin >> times;
-	char* ps = buildstr(ch, times);
-	cout << endl << ps;
-	delete[] ps;
-	ps = buildstr('+', times/2);
-	cout << ps << "-DONE-" << ps;
-	delete[] ps;
+	char ruler[len];
+	ruler[len - 1] = '\0';
+	ruler[0] = ruler[len - 2] = '|';
+	
+
+	for (int i = 1; i < len - 2; i++)
+	{
+		ruler[i] = ' ';
+	}
+	cout << ruler<<endl;
+	for (int i=1; i <= times; i++)
+	{
+		subdivide(ruler, 0, len - 2,i);
+		cout << ruler<<endl;
+		for (int k = 1; k < len - 2; k++)
+		{
+			ruler[k] = ' ';
+		}
+	}
 	return 0;
 }
 
-char* buildstr(char ch,int times)
+void subdivide(char ch[],int first,int end,int times)
 {
-	char* temp = new char[times + 1];
-	//为指针开辟内存，方便后续指针传递返回出去
-	int i = 0;
-	for (i; i < times; i++)
-	{
-		temp[i] = ch;
-	}
-	temp[times] = '\0';
-	cout << endl;
-	return(temp);
+	if (times == 0)
+		return;
+	int middle = (first + end) / 2;
+	ch[middle] = '|';
+	subdivide(ch, first, middle,times-1);
+	subdivide(ch, middle, end,times-1);	
 }
+
+
