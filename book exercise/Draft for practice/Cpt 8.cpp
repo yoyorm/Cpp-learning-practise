6015316583de7f7058d0ba0b7264ea5f6faed70f
yoyorm <yoyormiau1011@gmail.com>
2025-09-12 21:27:33 +0800

    更新：增加了第 8 章函数模板的草稿

--- book exercise/Draft for practice/Cpt 8.cpp
+++ book exercise/Draft for practice/Cpt 8.cpp
@@ -31,78 +31,104 @@ using namespace std;
 //}
 
 
+
+
+
 //使用函数的重载，截取字符或者数字的前几位
-char* cutch(const char ch[], int pos);
-int cutch(int num, int pos);
-const int Arsize = 50;
+//char* cutch(const char ch[], int pos);
+//int cutch(int num, int pos);
+//const int Arsize = 50;
+//int main()
+//{
+//	char sample[Arsize];
+//	int cut_pos = 1;
+//	int choice = 0;
+//	int number;
+//	cout << "Choose:\n1)number\t2)string\n";
+//	cin >> choice;
+//	switch (choice)
+//	{
+//	case 1:{
+//		cout << "Enter a number:";
+//		cin >> number;
+//		cout << "Enter the number to cut:";
+//		cin >> cut_pos;
+//		if (cut_pos <= 0)
+//			return -1;
+//		cout << "\n" << cutch(number, cut_pos);
+//		break;
+//	}
+//	case 2: {
+//		cout << "Enter a sentence:";
+//		cin >> sample;
+//		cout << "Enter the number to cut:";
+//		cin >> cut_pos;
+//		if (cut_pos <= 0)
+//			return -1;
+//		char* result = cutch(sample, cut_pos);
+//		cout << "\n" << cutch(sample, cut_pos);
+//		delete[] result;
+//		break;
+//	}
+//	}
+//	
+//
+//	return 0;
+//}
+//
+//char* cutch(const char ch[], int pos = 1)
+//{
+//	
+//	char* tempp = new char[pos+1];
+//	for (int i = 0; i < pos; i++)
+//	{
+//		tempp[i] = ch[i];
+//	}
+//	tempp[pos] = '\0';
+//	
+//	return tempp;
+//}
+//
+//int cutch(int num, int pos = 1)
+//{
+//	int num2 = num;
+//	int size = 1;
+//	while ((num /= 10) >= 1)
+//	{
+//		size++;
+//	}
+//	if (pos > size)
+//		return num2;
+//	else
+//	{
+//		for (int i = size; i > pos; i--)
+//		{
+//			num2 /= 10;
+//			
+//		}
+//		return num2;
+//	}
+//}
+
+
+//函数模板的创建
+template<typename T>
+void Swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
 int main()
 {
-	char sample[Arsize];
-	int cut_pos = 1;
-	int choice = 0;
-	int number;
-	cout << "Choose:\n1)number\t2)string\n";
-	cin >> choice;
-	switch (choice)
-	{
-	case 1:{
-		cout << "Enter a number:";
-		cin >> number;
-		cout << "Enter the number to cut:";
-		cin >> cut_pos;
-		if (cut_pos <= 0)
-			return -1;
-		cout << "\n" << cutch(number, cut_pos);
-		break;
-	}
-	case 2: {
-		cout << "Enter a sentence:";
-		cin >> sample;
-		cout << "Enter the number to cut:";
-		cin >> cut_pos;
-		if (cut_pos <= 0)
-			return -1;
-		char* result = cutch(sample, cut_pos);
-		cout << "\n" << cutch(sample, cut_pos);
-		delete[] result;
-		break;
-	}
-	}
 	
-
-	return 0;
-}
-
-char* cutch(const char ch[], int pos = 1)
-{
-	
-	char* tempp = new char[pos+1];
-	for (int i = 0; i < pos; i++)
-	{
-		tempp[i] = ch[i];
-	}
-	tempp[pos] = '\0';
-	
-	return tempp;
-}
-
-int cutch(int num, int pos = 1)
-{
-	int num2 = num;
-	int size = 1;
-	while ((num /= 10) >= 1)
-	{
-		size++;
-	}
-	if (pos > size)
-		return num2;
-	else
-	{
-		for (int i = size; i > pos; i--)
-		{
-			num2 /= 10;
-			
-		}
-		return num2;
-	}
+	int x = 12, y = 9;
+	cout << "Origin: " << "\nx = " << x << "\ty = " << y << endl;
+	Swap(x, y);
+	cout << "After swap: " << "\nx = " << x << "\ty = " << y<<endl;
+	double i = 5.5, j = 8.2;
+	cout << "Origin: " << "\ni = " << i << "\tj = " << j << endl;
+	Swap(i, j);
+	cout << "After swap: " << "\ni = " << i << "\tj = " << j << endl;
 }
